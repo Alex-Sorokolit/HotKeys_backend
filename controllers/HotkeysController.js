2c3853887e00abc9395b24d5e2f3f9c20836284e
Alex-Sorokolit <sorokolital@gmail.com>
2023-04-22 12:15:53 +0300

    start create hotkey controller

---
+++ controllers/HotkeysController.js
@@ -0,0 +1,11 @@
+const { HotKey } = require("../models/hotkey");
+
+class HotKeyController {
+  // Додавання hotkey
+  // Видалення hotkey
+  // Редагування hotkey
+  // Отримати всі hotkey by user
+}
+
+const HotKeyCtrl = new HotKeyController();
+module.exports = HotKeyCtrl;
